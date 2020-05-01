fd4197902eb35636c67f0f4a68e75a9f01cf32ad
Rene Avila <reneavila1993@gmail.com>
2020-05-01 17:54:35 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,15 +1,24 @@
+
+
 require_relative './part_1_solution.rb'
+require 'pry'
+
+#cart = [{:item=>"AVOCADO", :price=>3.0, :clearance=>true, :count=>2}]
+#coupons = [{:item=>"AVOCADO", :num=>2, :cost=>5.0}]
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  counter = 0 
+  while counter < coupons.length do 
+    cart_item = find_item_by_name
+  counter += 1
+  end
+end
+
+    
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+
 end
 
 def checkout(cart, coupons)
